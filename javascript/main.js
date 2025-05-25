ed22828360e146f80647c7555333baffc801088f
Mossyzz <starbamd14@gmail.com>
2025-05-25 18:30:15 +0700

    edit new file

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,3 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
-
-app.listen(PORT, () => {
-    console.log(`API Listening om PORT ${PORT}`)
-})
-
-app.get('/', (req, res) => {
-    res.send('This my API running');
-})
-
-module.exports = app
-
-
 const togglePassword = document.getElementById('openEye');
 const password = document.getElementById('password');
 
